5bbb408228601efa0fd9bbb8c96d184e88a9ed81
RealVishuSharma <vishusharmaa2211@gmail.com>
2024-03-29 10:25:13 +0530

    Added Environment Setup for FrontEnd!

---
+++ frontend/app/(main)/(routes)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex ">
+     <Button variant="destructive">
+      This is a button!
+      </Button>
+    </div>
+    </>
+  );
+}
---
+++ frontend/app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestaPage = () => {
+    return ( 
+        <div>
+            <p>Test Page</p>
+        </div>
+     );
+}
+ 
+export default TestaPage;;
